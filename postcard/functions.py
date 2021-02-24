cf343ec3ccda8b98e043cc0e955e04e6241a00d8
Master-Hash <A137294381b@163.com>
2021-02-25 00:37:52 +0800

    fix #6

--- postcard/functions.py
+++ postcard/functions.py
@@ -21,17 +21,27 @@ def today() -> str:
     return f"{a.year} 年 {a.month} 月 {a.day} 日 {a.strftime('%A')}"
 
 def getCity(ip: str) -> str:
+    logging.warning(f"{ip} 被访问")
     if ip == "127.0.0.1":
         # There's no place like 127.0.0.1，that's a famous photo
         return "本地"
     with geoip2.database.Reader("GeoLite2-City.mmdb") as reader:
-	    response = reader.city(ip)
+        response = reader.city(ip)
     if response.city.names:
-        return response.city.names['zh-CN']
+        if 'zh-CN' in response.city.names:
+            return response.city.names['zh-CN']
+        else:
+            return f" {response.city.name} "
     elif response.country.names:
-        return response.country.names['zh-CN']
+        if 'zh-CN' in response.country.names:
+            return response.country.names['zh-CN']
+        else:
+            return f" {response.country.name} "
     elif response.continent.names:
-        return response.continent.names['zh-CN']
+        if 'zh-CN' in response.country.names:
+            return response.continent.names['zh-CN']
+        else:
+            return f" {response.continent.name} "
     else:
         logging.warning(f"找不到 ip 位置：{ip}")
         return "神秘位置" # 这里用什么好呢
